57fbbc48a668c3d30cc437db3cb88c384afc60cd
Yuri Maineri <yurimaineri5@gmail.com>
2019-10-17 08:57:58 -0300

    Corrigindo banco para localhost

--- db/PDOFactory.php
+++ db/PDOFactory.php
@@ -5,18 +5,18 @@ class PDOFactory {
 
     // método estático para criação de conexão
     public static function getConexao() {
-        
+
         if(!isset($pdo)) {
-            /* usando o construtor do PDO devemos sempre adicionar a string 
+            /* usando o construtor do PDO devemos sempre adicionar a string
             relativa ao Sistema Gerenciador de Banco de Dados (SGBD) a ser utilizado */
             //              banco:host=nomehost;dbname=nomedobanco  usuário senha
-            $pdo = new PDO("mysql:host=mysql.yurimaineri.kinghost.net;dbname=yurimaineri","yurimaineri", "suporte2019");
+            $pdo = new PDO("mysql:host=localhost;dbname=app","root", "");
             // indicação de atributos de inicialização da conexão com o SGBD
             // reportar erros relativos ao controle de exceção
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             // indicação do modo padrão de retorno dos dados
             $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-            // conversão de valores numéricos para strings no retorno 
+            // conversão de valores numéricos para strings no retorno
             $pdo->setAttribute(PDO::ATTR_STRINGIFY_FETCHES, false);
             // emulação do "prepared statements"
             $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
